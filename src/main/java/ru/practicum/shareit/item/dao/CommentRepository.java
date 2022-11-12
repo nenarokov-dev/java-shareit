f605c65aecdf2696373a3bfc09ec414448edd3b1
Виктор <Vik2697@yandex.ru>
2022-11-12 18:01:23 +0300

    Внёс правки по ревью №1.

--- src/main/java/ru/practicum/shareit/item/dao/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/dao/CommentRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.item.dao;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.item.model.Comment;
 
